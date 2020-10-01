a7e0bfbb87aa28bda26088afff19469aa6f0b60a
niaakop <agniakopova@gmail.com>
2020-10-01 21:51:35 +0300

    without right_word_index

--- palindrom.rb
+++ palindrom.rb
@@ -1,14 +1,13 @@
-	def palindrome(word)
-		left_letter_index = 0
-		length = word.length
-		(length/2).times do
-			right_letter_index = length - left_letter_index - 1
-			if word[left_letter_index] != word[right_letter_index]
-				return false
-			end
-			left_letter_index += 1
+def palindrome(word)
+	left_letter_index = 0
+	length = word.length
+	(length/2).times do |left_letter_index|
+		if word[left_letter_index] != word[length - left_letter_index - 1]
+			return false
 		end
-		return true
+		left_letter_index += 1
 	end
+	return true
+end
 
-puts palindrome("abcddcba")
+puts palindrome("abcddba")
