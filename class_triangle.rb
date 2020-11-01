8a7bac71f198a3efa3f13c58798a1d3abff8594b
niaakop <agniakopova@gmail.com>
2020-11-01 21:13:46 +0300

    triangle

--- class_triangle.rb
+++ class_triangle.rb
@@ -1,16 +1,17 @@
 class Triangle
    attr_reader :defined
-   def initialize(side_a = 0, side_b = 0, side_c = 0,
-            ang_a = 0, ang_b = 0, ang_c = 0)
-   @defined = ((side_a != 0) & (side_b != 0) & (side_c != 0)) | 
-               ((side_a != 0) & (side_b != 0) & (ang_a != 0)) | 
-               ((side_a != 0) & (ang_a != 0) & (ang_b != 0))
+   def initialize(side_a:, side_b:, side_c:, ang_a:, ang_b:, ang_c:)
+   @defined = ((side_a != nil) & (side_b != nil) & (side_c != nil)) | 
+               ((side_a != nil) & (side_b != nil) & (ang_a != nil)) | 
+               ((side_a != nil) & (ang_a != nil) & (ang_b != nil))
    end
 end
 
-a = Triangle.new(1, 2, 3)
-b = Triangle.new(1, 2, 0, 4)
-c = Triangle.new(0, 0, 3, 4)
+#parameters = {side_a: nil, side_b: nil, side_c: nil,
+ #           ang_a: nil, ang_b: nil, ang_c: nil}
+a = Triangle.new({side_a: 14, side_b: 51, side_c: 543})
+#b = Triangle.new()
+#c = Triangle.new()
 puts a.defined # => true
-puts b.defined # => true
-puts c.defined # => false
+#puts b.defined # => true
+#puts c.defined # => false
