7a50f3521ab95c090669d4a8a0dca59034a06a40
niaakop <agniakopova@gmail.com>
2020-11-01 22:18:35 +0300

    triangle

--- class_triangle.rb
+++ class_triangle.rb
@@ -1,10 +1,25 @@
 class Triangle
-   attr_reader :defined
-   def initialize(side_a: nil, side_b: nil, side_c: nil, ang_a: nil, ang_b: nil, ang_c: nil)
-   @defined = ((side_a != nil) & (side_b != nil) & (side_c != nil)) | 
-               ((side_a != nil) & (side_b != nil) & (ang_a != nil)) | 
-               ((side_a != nil) & (ang_a != nil) & (ang_b != nil))
-   end
+    attr_reader :defined
+    def initialize(side_a: nil, side_b: nil, side_c: nil, 
+                        ang_a: nil, ang_b: nil, ang_c: nil)
+    @side_a = side_a
+    @side_b = side_b
+    @side_c = side_c
+    @ang_a = ang_a
+    @ang_b = ang_b
+    @ang_a = ang_c
+    @defined = three_sides || two_sides_one_angle || one_side_two_angs
+    end
+    private
+    def three_sides
+        ((@side_a != nil) & (@side_b != nil) & (@side_c != nil))
+    end
+    def two_sides_one_angle
+        ((@side_a != nil) & (@side_b != nil) & (@ang_a != nil)) 
+    end
+    def one_side_two_angs
+        ((@side_a != nil) & (@ang_a != nil) & (@ang_b != nil))
+    end
 end
 
 a = Triangle.new(side_a: 14, side_b: 51, side_c: 543)
