52cd0ca227d2bbbb3f38b2d4c71d08ac48baf021
niaakop <agniakopova@gmail.com>
2020-11-01 21:35:05 +0300

    triangle

--- class_triangle.rb
+++ class_triangle.rb
@@ -1,17 +1,12 @@
 class Triangle
    attr_reader :defined
-   def initialize(side_a:, side_b:, side_c:, ang_a:, ang_b:, ang_c:)
+   def initialize(side_a: nil, side_b: nil, side_c: nil, ang_a: nil, ang_b: nil, ang_c: nil)
    @defined = ((side_a != nil) & (side_b != nil) & (side_c != nil)) | 
                ((side_a != nil) & (side_b != nil) & (ang_a != nil)) | 
                ((side_a != nil) & (ang_a != nil) & (ang_b != nil))
    end
 end
 
-#parameters = {side_a: nil, side_b: nil, side_c: nil,
- #           ang_a: nil, ang_b: nil, ang_c: nil}
 a = Triangle.new(side_a: 14, side_b: 51, side_c: 543)
-#b = Triangle.new()
-#c = Triangle.new()
+
 puts a.defined # => true
-#puts b.defined 
-#puts c.defined 
