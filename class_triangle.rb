27065df7f1a82ec3b53906c7977b2987ff213ca1
niaakop <agniakopova@gmail.com>
2020-11-15 23:32:53 +0300

    Triangle

--- class_triangle.rb
+++ class_triangle.rb
@@ -14,15 +14,15 @@ class Triangle
 
     private
     def three_sides
-        (@side_a != nil) && (@side_b != nil) && (@side_c != nil)
+        (@side_a.is_a? Integer) && (@side_b.is_a? Integer) && (@side_c.is_a? Integer)
     end
 
     def two_sides_one_ang
-        (@side_a != nil) && (@side_b != nil) && (@ang_a != nil)
+        (@side_a.is_a? Integer) && (@side_b.is_a? Integer) && (@ang_a.is_a? Integer)
     end
 
     def one_side_two_angs
-        (@side_a != nil) && (@ang_a != nil) && (@ang_b != nil)
+        (@side_a.is_a? Integer) && (@ang_a.is_a? Integer) && (@ang_b.is_a? Integer)
     end
 end
 
