1fec0c63caac968acfc6d1d50037d35473e7db61
niaakop <agniakopova@gmail.com>
2021-01-19 23:10:03 +0300

    .

--- class_triangle_axis.rb
+++ class_triangle_axis.rb
@@ -79,18 +79,18 @@ end
 
 first_point = Point.new(2, 4)
 second_point = Point.new(3, 6)
-my_first_line = Line.new(var1: first_point, var2: second_point)
+first_line = Line.new(var1: first_point, var2: second_point)
 
 third_point = Point.new(4, 8)
-puts third_point.is_on?(my_first_line) #=> true
+puts third_point.is_on?(first_line) #=> true
 
-my_second_line = Line.new(var1: 0, var2: 2, var3: 0.5)
+second_line = Line.new(var1: 0, var2: 2, var3: 0.5)
 fourth_point = Point.new(-0.25, 30)
-puts fourth_point.is_on?(my_second_line) #=> true
+puts fourth_point.is_on?(second_line) #=> true
 
 #####
 a = Point.new(26, 6)
-b = Point.new(25, 6)
+b = Point.new(27, 6)
 c = Point.new(14, 7)
 abc = Triangle.new(a: a, b: b, c: c)
 puts abc.exist #=>true
