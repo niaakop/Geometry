fda74a18533b9fc3bebc1933aec6e7605179e351
niaakop <agniakopova@gmail.com>
2021-03-20 17:44:37 +0300

    cleaning

--- class_triangle.rb
+++ class_triangle.rb
@@ -64,11 +64,7 @@ class Point
   end
 
   def !=(point)
-    # if !point.is_a?(Point)
-    #   true
-    # else
       !(self == point)
-    # end
   end
 end
 
@@ -114,15 +110,14 @@ second_point = Point.new(3, 6)
 first_line = Line.new(var1: first_point, var2: second_point)
 
 third_point = Point.new(4, 8)
-puts third_point.is_on?(first_line) #=> true
+puts third_point.is_on?(first_line) # true
 
 second_line = Line.new(var1: 0, var2: 2, var3: 0.5)
 fourth_point = Point.new(-0.25, 30)
-puts fourth_point.is_on?(second_line) #=> true
+puts fourth_point.is_on?(second_line) # true
 
-#####
 a = Point.new(26, 6)
-b = Point.new(27, 6)
+b = Point.new(27, 6) 
 c = Point.new(14, 7)
 abc = Triangle.new(a: a, b: b, c: c)
-puts abc.exist #=>true
+puts abc.exist # true
