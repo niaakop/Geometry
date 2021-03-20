f4b2e3539465395c95bd160e48ad2de9f76b9998
niaakop <agniakopova@gmail.com>
2021-03-20 17:50:41 +0300

    added explicit conversion to string

--- geometry_error.rb
+++ geometry_error.rb
@@ -5,7 +5,7 @@ class IncompatibleParamsError < GeometryError
   attr_reader :cls
   def initialize(cls) 
     @cls = cls
-    super("Incompatible Params Error. #{cls} class.")
+    super("Incompatible Params Error. #{cls.to_s} class.")
   end
 end
 
@@ -13,6 +13,6 @@ class DegenerateShapeError < GeometryError
   attr_reader :cls
   def initialize(cls) 
     @cls = cls
-    super("Degenerate Shape Error. #{cls} class.")
+    super("Degenerate Shape Error. #{cls.to_s} class.")
   end
 end
