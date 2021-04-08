83a5f9b6d44cafb192961ead66ba6c984b305242
nstkpv <nst.kpv@gmail.com>
2021-04-08 10:23:32 +0300

    Add dir Geometry + move all classes into separate files in this dir

--- geometry/line.rb
+++ geometry/line.rb
@@ -1,8 +1,12 @@
-class Line
+require_relative './array.rb'
+require_relative './error.rb'
+require_relative './point.rb'
+
+class Geometry::Line
   attr_reader :a, :b, :c, :exist
 
   def initialize(var1: nil, var2: nil, var3: nil) # var1 is 1st point or a, var2 is 2nd point or b, var3 is c
-    if [var1, var2].are_kind_of?(Point)
+    if [var1, var2].are_kind_of?(Geometry::Point)
       if var1 != var2
         @exist = true
         @a = var2.x - var1.x
@@ -10,7 +14,7 @@ class Line
         @c = var1.x * var2.y - var2.x * var1.y
       else
         @exist = false
-        raise DegenerateShapeError.new(self.class)
+        raise Geometry::DegenerateShapeError.new(self.class)
       end
     elsif [var1, var2, var3].are_kind_of?(Integer, Float)
       if !(var1 == 0 && var2 == 0)
@@ -20,11 +24,11 @@ class Line
         @c = var3
       else
         @exist = false
-        raise DegenerateShapeError.new(self.class)
+        raise Geometry::DegenerateShapeError.new(self.class)
       end
     else
       @exist = false
-      raise IncompatibleParamsError.new(self.class)
+      raise Geometry::IncompatibleParamsError.new(self.class)
     end
   end
 end
--- geometry/point.rb
+++ geometry/point.rb
@@ -1,4 +1,7 @@
-class Point
+require_relative './error.rb'
+require_relative './line.rb'
+
+class Geometry::Point
   attr_reader :exist, :x, :y
 
   def initialize(x, y)
@@ -8,12 +11,12 @@ class Point
       @exist = true
     else
       @exist = false
-      raise IncompatibleParamsError.new(self.class)
+      raise Geometry::IncompatibleParamsError.new(self.class)
     end
   end
 
   def is_on?(line)
-    if line.is_a?(Line)
+    if line.is_a?(Geometry::Line)
       @y * line.a + @x * line.b + line.c == 0
     else
       false
@@ -21,7 +24,7 @@ class Point
   end
 
   def ==(point)
-    if point.is_a?(Point)
+    if point.is_a?(Geometry::Point)
       @x == point.x && @y == point.y
     else
       false
